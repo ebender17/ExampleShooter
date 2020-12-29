6f3272cfa598b78e27e51cdbd360ea1224b4b9e6
Emily Bender <bender.emily1@gmail.com>
2020-12-29 17:03:35 -0500

    Project Setup

---
+++ Source/ExampleShooter/ExampleShooterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ExampleShooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EXAMPLESHOOTER_API AExampleShooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
