0131cd56fa9224aeb4b3c4f2b3a2576195294e05
Emily Bender <bender.emily1@gmail.com>
2021-01-02 15:40:48 -0500

    Spawn Gun Actor at Runtime to Shooter Character & Spawn Particle Effects on Gun Shoot

--- Source/ExampleShooter/ShooterCharacter.h
+++ Source/ExampleShooter/ShooterCharacter.h
@@ -6,6 +6,7 @@
 #include "GameFramework/Character.h"
 #include "ShooterCharacter.generated.h"
 
+class AGun;
 UCLASS()
 class EXAMPLESHOOTER_API AShooterCharacter : public ACharacter
 {
@@ -30,7 +31,14 @@ private:
 	void MoveRight(float AxisValue); 
 	void LookUpRate(float AxisValue);
 	void LookRightRate(float AxisValue);
+	void Shoot();
 
 	UPROPERTY(EditAnywhere)
 	float RotationRate = 10.f;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<AGun> GunClass; 
+
+	UPROPERTY()
+	AGun* Gun;
 };
