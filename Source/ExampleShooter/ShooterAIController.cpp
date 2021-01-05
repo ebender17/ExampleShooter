56fd7639233c2ccf58099831c6362c82e1a0fbb0
Emily Bender <bender.emily1@gmail.com>
2021-01-05 15:39:07 -0500

    Basic AI Implementation

---
+++ Source/ExampleShooter/ShooterAIController.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterAIController.h"
+#include "Kismet/GameplayStatics.h"
+
+void AShooterAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void AShooterAIController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+	//Not best implementation
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+
+	if (LineOfSightTo(PlayerPawn))
+	{
+		SetFocus(PlayerPawn);
+		MoveToActor(PlayerPawn, AcceptanceRadius);
+	}
+	else
+	{
+		ClearFocus(EAIFocusPriority::Gameplay); 
+		StopMovement(); 
+	}
+}
+
